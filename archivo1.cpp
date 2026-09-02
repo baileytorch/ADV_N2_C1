5c852103c54d4d0e5eb3af5e5df189cd48ad3568
baileytorch <baileytorch@hotmail.com>
2026-09-02 10:27:00 -0400

    codigo inicial

--- archivo1.cpp
+++ archivo1.cpp
@@ -1,9 +1,51 @@
 #include <iostream>
+#include <windows.h>
 using namespace std;
 
 int main(){
+    // Esto es un comentario de 1 línea
+    /*
+    Esto es un comentario de múltiples líneas
+    */
 
+    // Configurar la salida de la consola a UTF-8, nuestro set de caracteres
+    SetConsoleOutputCP(CP_UTF8);
+
+    // Definición de nuestro Personaje
+    string nombreDelfin = "Delfin Quispe";
+    int vidaDelfin = 500;
+    int edadDelfin = 48;
+    bool esAlcaldeDelfin = false;
+    float ratingDelfin = 2.36;
+
+    // Definición del Enemigo
+    string nombreTigresa = "Tigresa del Oriente";
+    int vidaTigresa = 500;
+    int edadTigresa = 80;
+    bool esAlcaldeTigresa = false;
+    float ratingTigresa = 4.0;
+
+    // Con esta línea muestro un mensaje mediante el terminal
     cout << "Buen día queridos estudiantes" << endl;
 
+    cout << "Ficha Técnica Inicial" << endl;
+    cout << "=============" << endl;
+    cout << "Nombre Personaje: " << nombreDelfin << endl;
+    cout << "Vida Personaje: " << vidaDelfin << endl;
+    cout << "Edad Personaje: " << edadDelfin << endl;
+    cout << "Es alcalde: " << esAlcaldeDelfin << endl;
+    cout << "Rating: " << ratingDelfin << endl;
+
+    int vidaFinal = 0;
+    vidaFinal = vidaDelfin - ratingTigresa;
+    vidaDelfin = vidaFinal;
+
+    cout << "Ficha Técnica Final" << endl;
+    cout << "=============" << endl;
+    cout << "Nombre Personaje: " << nombreDelfin << endl;
+    cout << "Vida Personaje: " << vidaDelfin << endl;
+    cout << "Edad Personaje: " << edadDelfin << endl;
+    cout << "Es alcalde: " << esAlcaldeDelfin << endl;
+    cout << "Rating: " << ratingDelfin << endl;
     return 0;
 }
