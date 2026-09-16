cd5047dee6b1776b920e00a3099c39cdf0e2479b
Erick Bailey <baileytorch@hotmail.com>
2026-09-16 11:25:10 -0300

    Aventuras POO

---
+++ aventurasPoo.cpp
@@ -0,0 +1,83 @@
+#include <iostream>
+#include <windows.h>
+#include <cstdlib>
+using namespace std;
+
+int vida = 100;
+bool vivo = true;
+
+void avanzar(){
+    cout << "Poo avanza..." << endl;
+}
+
+void saltar(){
+    cout << "Poo salta..." << endl;
+}
+
+void recibirDanio(int danio){
+    // vida = vida - danio;
+    // Forma abreviada de realizar una operación aritmética de suma o resta
+    vida -= danio;
+
+    if (vida < 0) {
+        vida = 0;
+        vivo = false;
+    }
+
+    cout << "Poo recibió " << danio << " de daño." << endl;
+    cout << "Su vida restante es " << vida << "." << endl;
+}
+
+void verEstado(){
+    string alerta = "";
+    if (0 < vida < 30) alerta = "Vida demasiado baja!";
+
+    cout << "Estado de Poo" << endl;
+    cout << "Vida restante: " << vida << endl;
+    cout << "Está Vivo? " << (vivo == true ? "Si": "No") << endl;
+    cout << alerta << endl;
+}
+
+int main(){
+    SetConsoleOutputCP(CP_UTF8);
+
+    int opcion = 0;
+
+    while (opcion != 5 && vivo)
+    {
+        cout << "Selecciones su opción" << endl;
+        cout << "[1] Avanzar." << endl;
+        cout << "[2] Saltar." << endl;
+        cout << "[3] Recibir Daño." << endl;
+        cout << "[4] Revisar Estado Poo." << endl;
+        cout << "[5] Salir." << endl;
+        cin >> opcion;
+
+        switch (opcion)
+        {
+            case 1:
+                avanzar();
+                break;
+            case 2:
+                saltar();
+                break;
+            case 3:
+                int danio = 0;
+                cin >> danio;
+                recibirDanio(danio);
+                break;
+            case 4:
+                verEstado();
+                break;
+            case 5:
+                cout << "Saliendo..." << endl;
+                exit(0);
+                break;
+            default:
+                cout << "Opción ingresada NO corresponde...\nIntente nuevamente..." << endl;
+        }
+    }
+    
+
+    return 0;
+}
---
+++ tempCodeRunnerFile.cpp
@@ -0,0 +1,6 @@
+    while (vidaDelfin > 0)
+    {
+        vidaFinal = vidaDelfin - ratingTigresa;
+        vidaDelfin = vidaFinal;
+        cout << "Vida Delfin: " << vidaDelfin << endl;
+    }
